7e9bc11d6552824c931033380f29fb9c59015a5c
zevsus <z1evs71@gmail.com>
2025-03-01 19:11:04 +0300

    fix crash

--- mobile/app/src/main/java/ru/prodcontest/booq/presentation/MainActivity.kt
+++ mobile/app/src/main/java/ru/prodcontest/booq/presentation/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import ru.prodcontest.booq.presentation.navigation.NavGraph
 import ru.prodcontest.booq.presentation.theme.BooqTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- mobile/app/src/main/java/ru/prodcontest/booq/presentation/auth/login/LoginScreen.kt
+++ mobile/app/src/main/java/ru/prodcontest/booq/presentation/auth/login/LoginScreen.kt
@@ -19,53 +19,53 @@ object LoginScreenDestination
 
 @Composable
 fun LoginScreen(
-    viewModel: LoginViewModel = viewModel()
+    viewModel: LoginViewModel = hiltViewModel()
 ) {
 //    val viewState by viewModel.viewState.collectAsState()
 
-//    val viewState = viewModel.viewState.value
-//    var email by remember { mutableStateOf("") }
-//    var password by remember { mutableStateOf("") }
-//
-//    val emailValid by remember { derivedStateOf { Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}").matches(email) } }
-//    val passwordValid by remember { derivedStateOf { Regex("[a-zA-Z0-9\$&+,:;=?@#|'<>.^*()%!-]{8,}").matches(password) } }
-//
-//    Box(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .padding(16.dp),
-//        contentAlignment = Alignment.Center
-//    ) {
-//        Column(
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
-//            AuthTextField(
-//                value = email,
-//                onValueChange = { email = it },
-//                placeholder = "Email",
-//                isLocked = viewState.isLoading,
-//                iconResId = R.drawable.mail_24
-//            )
-//
-//            Spacer(modifier = Modifier.height(16.dp))
-//
-//            AuthTextField(
-//                value = password,
-//                onValueChange = { password = it },
-//                placeholder = "Password",
-//                isPassword = true,
-//                isLocked = viewState.isLoading,
-//                iconResId = R.drawable.key_24
-//            )
-//
-//            Spacer(modifier = Modifier.height(24.dp))
-//
-//            AuthButton(
-//                text = "Войти",
-//                onClick = { viewModel.login(email, password) },
-//                isLoaded = viewState.isLoading,
-//                modifier = Modifier.padding(top = 16.dp)
-//            )
-//        }
-//    }
+    val viewState = viewModel.viewState.value
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val emailValid by remember { derivedStateOf { Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}").matches(email) } }
+    val passwordValid by remember { derivedStateOf { Regex("[a-zA-Z0-9\$&+,:;=?@#|'<>.^*()%!-]{8,}").matches(password) } }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AuthTextField(
+                value = email,
+                onValueChange = { email = it },
+                placeholder = "Email",
+                isLocked = viewState.isLoading,
+                iconResId = R.drawable.mail_24
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            AuthTextField(
+                value = password,
+                onValueChange = { password = it },
+                placeholder = "Password",
+                isPassword = true,
+                isLocked = viewState.isLoading,
+                iconResId = R.drawable.key_24
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            AuthButton(
+                text = "Войти",
+                onClick = { viewModel.login(email, password) },
+                isLoaded = viewState.isLoading,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+    }
 }
