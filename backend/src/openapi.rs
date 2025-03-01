5d1a001df3f757feedf5d26cc2a52e942b61c130
CyberFather <maks12ate@gmail.com>
2025-03-01 22:51:42 +0300

    add: openapi spec to upload_document route

--- backend/src/openapi.rs
+++ backend/src/openapi.rs
@@ -1,13 +1,16 @@
 use crate::routes::{
     admin::routes::{__path_delete_user, __path_patch_user, __path_verify_guest},
-    users::routes::{__path_login, __path_patch_profile, __path_profile, __path_register},
+    users::{
+        routes::{__path_login, __path_patch_profile, __path_profile, __path_register},
+        validate::__path_upload_document,
+    },
 };
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
 #[openapi(
     paths(
-        login, register, profile, patch_profile,
+        login, register, profile, patch_profile, upload_document,
         verify_guest, delete_user, patch_user
     ),
     tags(
--- backend/src/routes/users/validate.rs
+++ backend/src/routes/users/validate.rs
@@ -12,6 +12,7 @@ use crate::{
     AppState,
 };
 
+#[utoipa::path(post, tag = "User", path = "/user/upload_document")]
 pub async fn upload_document(
     headers: HeaderMap,
     State(state): State<AppState>,
