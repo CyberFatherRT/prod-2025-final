a2744a2f18c17c5cf58639b607e7a6dc0d16f5c9
JustMarfix <rezak3031@ya.ru>
2025-03-01 21:57:04 +0300

    hotfix

--- backend/src/routes/admin/mod.rs
+++ backend/src/routes/admin/mod.rs
@@ -1,16 +1,16 @@
+use crate::{middlewares::auth_admin, AppState};
+use axum::routing::patch;
 use axum::{
     routing::{delete, post},
     Router,
 };
-
-use crate::{middlewares::auth_admin, AppState};
 
 pub mod routes;
 pub fn get_routes(state: AppState) -> Router {
     Router::new()
         .route("/verify_guest/{user_id}", post(routes::verify_guest))
         .route("/user/{user_id}", delete(routes::delete_user))
-        .route("/user/{user_id}", delete(routes::patch_user))
+        .route("/user/{user_id}", patch(routes::patch_user))
         .with_state(state)
         .layer(axum::middleware::from_fn(auth_admin))
 }
