926da35377a6729a280378a319b7c0ddeeb09db6
NinVoido <ninvoido.0@gmail.com>
2025-03-01 17:09:46 +0300

    add some admin routes

--- backend/src/routes/admin/mod.rs
+++ backend/src/routes/admin/mod.rs
@@ -5,4 +5,6 @@ use crate::AppState;
 mod routes;
 pub fn get_routes(state: AppState) -> Router {
     Router::new()
+        .route("/verify-gueroutes::verify_guest)
+        .with_state(state)
 }
--- backend/src/routes/admin/routes.rs
+++ backend/src/routes/admin/routes.rs
@@ -1,1 +1,16 @@
+use axum::{extract::Path, extract::State, http::HeaderMap, Json};
+use axum_macros::debug_handler;
+use uuid::Uuid;
 
+use crate::{db::Db, errors::ProdError, forms::admin::VerifyGuestForm, AppState};
+
+#[debug_handler]
+pub async fn verify_guest(
+    headers: HeaderMap,
+    Path(user_id): Path<Uuid>,
+    State(state): State<AppState>,
+) -> Result<(), ProdError> {
+    let mut conn = state.pool.conn().await?;
+
+    Ok(())
+}
