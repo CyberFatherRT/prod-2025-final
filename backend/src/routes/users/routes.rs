9f6ba49c4051c6359aa05927c9f7cf6ae70c6771
CyberFather <maks12ate@gmail.com>
2025-03-01 16:06:50 +0300

    fix: wrong response type

--- backend/src/routes/users/routes.rs
+++ backend/src/routes/users/routes.rs
@@ -1,13 +1,15 @@
-use axum::{extract::State, http::HeaderMap};
+use axum::{extract::State, http::HeaderMap, Json};
+use axum_macros::debug_handler;
 
 use crate::{
     db::Db, errors::ProdError, forms::users::ProfileResponseForm, models::RoleModel, AppState,
 };
 
+#[debug_handler]
 pub async fn profile(
     headers: HeaderMap,
     State(state): State<AppState>,
-) -> Result<ProfileResponseForm, ProdError> {
+) -> Result<Json<ProfileResponseForm>, ProdError> {
     let mut conn = state.pool.conn().await?;
 
     let _ = sqlx::query_as!(
