2646fab686ef477c20e1d99b21e327495bb9ad3b
CyberFather <maks12ate@gmail.com>
2025-03-02 14:07:06 +0300

    impl: deletion of booking

--- backend/src/routes/booking/mod.rs
+++ backend/src/routes/booking/mod.rs
@@ -1,10 +1,14 @@
 use crate::AppState;
-use axum::{routing::post, Router};
+use axum::{
+    routing::{delete, post},
+    Router,
+};
 
 pub mod routes;
 
 pub fn get_routes(state: AppState) -> Router {
     Router::new()
         .route("/create", post(routes::create_booking))
+        .route("/{booking_id}", delete(routes::delete_booking))
         .with_state(state)
 }
--- backend/src/routes/booking/routes.rs
+++ backend/src/routes/booking/routes.rs
@@ -1,8 +1,10 @@
 use axum::{
-    extract::State,
+    extract::{Path, State},
     http::{HeaderMap, StatusCode},
     Json,
 };
+use sqlx::Acquire;
+use uuid::Uuid;
 
 use crate::{
     db::Db,
@@ -49,6 +51,36 @@ pub async fn create_booking(
         ));
     }
 
+    let mut tx = conn.begin().await?;
+
+    let bookable = sqlx::query!(
+        r#"
+        SELECT i.bookable
+        FROM coworking_items ci
+        JOIN items i ON i.id = ci.item_id
+        WHERE ci.id = $1 AND ci.coworking_id = $2
+    "#,
+        form.coworking_item_id,
+        form.coworking_id
+    )
+    .fetch_one(tx.as_mut())
+    .await
+    .map(|record| record.bookable)
+    .map_err(|err| match err {
+        sqlx::Error::RowNotFound => ProdError::NotFound(format!(
+            "No item was found with that uuid - {}",
+            form.coworking_item_id
+        )),
+        _ => ProdError::DatabaseError(err),
+    })?;
+
+    if !bookable {
+        return Err(ProdError::Forbidden(format!(
+            "You can not book item with that id - {}",
+            form.coworking_item_id,
+        )));
+    }
+
     let booking = sqlx::query_as!(
         BookingModel,
         r#"
@@ -63,12 +95,62 @@ pub async fn create_booking(
         form.time_start,
         form.time_end
     )
-    .fetch_one(conn.as_mut())
+    .fetch_one(tx.as_mut())
     .await
     .map_err(|err| match err {
         sqlx::Error::Database(e) if e.is_foreign_key_violation() => ProdError::ShitHappened("Coworking or coworking_item not found".to_string()),
         _ => ProdError::DatabaseError(err)
     })?;
 
+    tx.commit().await?;
+
     Ok((StatusCode::CREATED, Json(booking)))
+}
+
+/// Delete booking
+#[utoipa::path(
+    delete,
+    tag = "Bookings",
+    path = "/booking/{booking_id}",
+    responses(
+        (status = 204, description = "Booking was successully deleted"),
+        (status = 403, description = "User doesn't own that booking"),
+        (status = 404, description = "No booking was found with booking_id"),
+    )
+)]
+pub async fn delete_booking(
+    header: HeaderMap,
+    State(state): State<AppState>,
+    Path(booking_id): Path<Uuid>,
+) -> Result<StatusCode, ProdError> {
+    let mut conn = state.pool.conn().await?;
+    let claim = claims_from_headers(&header)?;
+
+    let mut tx = conn.begin().await?;
+
+    let (user_id, company_id) = sqlx::query!(
+        r#"
+        DELETE FROM bookings
+        WHERE id = $1
+        RETURNING user_id, company_id
+        "#,
+        booking_id
+    )
+    .fetch_one(tx.as_mut())
+    .await
+    .map(|record| (record.user_id, record.company_id))
+    .map_err(|err| match err {
+        sqlx::Error::RowNotFound => ProdError::NotFound("No booking was found".to_string()),
+        _ => ProdError::DatabaseError(err),
+    })?;
+
+    if claim.user_id != user_id || claim.company_id != company_id {
+        return Err(ProdError::Forbidden(
+            "You don't have permission to that booking".to_string(),
+        ));
+    }
+
+    tx.commit().await?;
+
+    Ok(StatusCode::NO_CONTENT)
 }
