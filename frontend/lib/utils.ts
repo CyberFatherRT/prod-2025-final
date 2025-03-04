b32c99cf3ef98709a656312a5c0170d117301f51
CyberFather <maks12ate@gmail.com>
2025-03-04 05:19:18 +0300

    fix: backendDomain

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const backendDomain = "https://prod-team-13-cltnksuj.final.prodcontest.ru";
+export const backendDomain = "https://prod-team-13-cltnksuj.final.prodcontest.ru/backend_api";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
